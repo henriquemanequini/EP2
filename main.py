0b4209be981540d4fc3f3d02992b7a2375c50c60
henriquemanequini <henriquem5@al.insper.edu.br>
2022-05-11 16:51:31 -0300

    Página inicial

---
+++ main.py
@@ -0,0 +1,12 @@
+#Página inicial
+print(' ============================ ')
+print('|                            |')
+print('| Bem-vindo ao Insper Países |')
+print('|                            |')
+print(' ==== Design de Software ==== \n')
+
+print('Comandos:\n' '   dica       - entra no mercado de dicas\n' '   desisto    - desiste da rodada\n' '   inventario - exibe sua posição\n')
+
+print('Um país foi escolhido, tente adivinhar!') 
+print('Você tem 20 tentativa(s)')
+print('Qual seu palpite?')
