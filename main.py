9cb15bef9a8ac972fa321507b7e46b3d526b798c
manoelabms <manoelabusatto04@gmail.com>
2022-05-11 19:47:15 -0300

    pondo a funcao de haversine

--- main.py
+++ main.py
@@ -44,19 +44,19 @@ while chances > 0:
         print('----------------------------------------')
         opcao_dica = input('Escolha sua opção [0|1|2|3|4]: ')
         respostas_dica = ['0','1','2','3','4','5']
-    while opcao_dica not in respostas_dica:
-        print ('Opção inválida')
-        opcao_dica = input('Escolha sua opção [0|1|2|3|4]: ')
-    if opcao_dica == '1':
-        chances -= 3
-    elif opcao_dica == '2':
-        chances -= 2
-    elif opcao_dica == '3':
-        chances -= 5
-    elif opcao_dica == '4':
-        chances -= 4
-    elif opcao_dica == '0':
-        palpite = input('Qual seu palpite? ')
+        while opcao_dica not in respostas_dica:
+            print ('Opção inválida')
+            opcao_dica = input('Escolha sua opção [0|1|2|3|4]: ')
+        if opcao_dica == '1':
+            chances -= 3
+        elif opcao_dica == '2':
+            chances -= 2
+        elif opcao_dica == '3':
+            chances -= 5
+        elif opcao_dica == '4':
+            chances -= 4
+        elif opcao_dica == '0':
+            palpite = input('Qual seu palpite? ')
 
     elif palpite not in DADOS.keys():
         print('país desconhecido')
@@ -67,7 +67,7 @@ while chances > 0:
         chances = 0 #isso faz com que saia do while e termine o código
 
     else: 
-        #latitude 
+        #latitude e longitude
         for pais, dados in DADOS.items():
             if pais == resposta_certa:
                 for item, especificidades in pais.items():
@@ -75,8 +75,15 @@ while chances > 0:
                         especificidades = x1
                     if item == 'longitude':
                         especificidades = y1
-            if pais == 
-
+            if pais == palpite:
+                for item2, especificidades2 in pais.items():
+                    if item2 == 'latitude':
+                        especificidades2 = x2
+                    if item2 == 'longitude':
+                        especificidades2 = y2
         def haversine(raioterra, x1, y1, x2, y2):
-    d = 2*r*asin(((sin((radians(x2)-radians(x1))/2))**2 + cos(radians(x1))*cos(radians(x2))*(sin((radians(y2)-radians(y1))/2)**2))**(1/2))
-    return d
+            d = 2*r*asin(((sin((radians(x2)-radians(x1))/2))**2 + cos(radians(x1))*cos(radians(x2))*(sin((radians(y2)-radians(y1))/2)**2))**(1/2))
+            return d
+        lista_distancia.append('{} km -> {}'.format(d, palpite))
+        print('Distâncias: \n {}'.format(lista_distancia))
+        
