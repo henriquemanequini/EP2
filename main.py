b13a6906be28f3b770ee046e86268a79d460e64c
manoelabms <manoelabusatto04@gmail.com>
2022-05-11 17:03:14 -0300

    selecionando pais dentro do dicionario

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-#Página inicial
+#importando bibliotecas necessárias
+import random
+from math import *
+from dadosnormalizados import DADOS
+
+#página inicial
 print(' ============================ ')
 print('|                            |')
 print('| Bem-vindo ao Insper Países |')
 print('|                            |')
 print(' ==== Design de Software ==== \n')
-
 print('Comandos:\n' '   dica       - entra no mercado de dicas\n' '   desisto    - desiste da rodada\n' '   inventario - exibe sua posição\n')
-
 print('Um país foi escolhido, tente adivinhar!') 
 print('Você tem 20 tentativa(s)')
+
+#inicializando palpite e selecionando aleatoriamente um pais
 palpite = input('Qual seu palpite?')
+correto = random.choice(DADOS.keys())
+
+
