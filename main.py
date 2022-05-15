ef45b980ffa9531026f4384be51fb7c3606af8d3
henriquemanequini <henriquem5@al.insper.edu.br>
2022-05-15 17:57:31 -0300

    dicas

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from msilib.schema import _Validation_records
 import random
 from math import *
 from dadosnormalizados import DADOS
-import funcoes 
+import funções 
 
 #dados e listas necessários
 lista_de_paises =[]
 lista_distancia = []
 lista_dicas = []
+lista_letras_capital = []
 for pais in DADOS.keys():
     lista_de_paises.append(pais)
 
@@ -27,7 +28,7 @@ print('Comandos:\n' '   dica       - entra no mercado de dicas\n' '   desisto   
 print('Um país foi escolhido, tente adivinhar!') 
 print('Você tem {} tentativa(s)'.format(chances))
 
-#inicializando palpite
+#inicializando palpite e selecionando aleatoriamente um pais
 palpite = input('Qual seu palpite? ')
 
 while chances > 0:
@@ -36,15 +37,6 @@ while chances > 0:
         desistencia = input('Tem certeza que deseja desistir da rodada? [s|n]')
         if desistencia == 's':
             print('>>> Que deselegante desistir, o país era:{}'.format(resposta_certa))
-            rodada_nova = input('Jogar novamente? [s|n]')
-            if rodada_nova == 's':
-                chances = 20
-                resposta_certa = random.choice(lista_de_paises)
-                print('Um país foi escolhido, tente adivinhar!') 
-                print('Você tem {} tentativa(s)'.format(chances))
-                palpite = input('Qual seu palpite? ')
-            else:
-                break
         else:
             palpite = input('Qual seu palpite? ')
 
@@ -62,34 +54,39 @@ while chances > 0:
             print('5. Continente       - custa 7 tentativas')
             print('0. Sem dica')
             print('----------------------------------------')
-            opcao_dica = input('Escolha sua opção [0|1|2|3|4|5]: ')
-            respostas_dica = ['0','1','2','3','4','5']
-            while opcao_dica not in respostas_dica:
-                print ('Opção inválida')
-                opcao_dica = input('Escolha sua opção [0|1|2|3|4|5]: ')
-
-        # CASO 2: o jogador ja pediu a dica de área, somente ela
-        elif 'Área' in lista_dicas and 'População'  not in lista_dicas and 'Continente' not in lista_dicas:
-            print('Mercado de Dicas')
-            print('----------------------------------------')
-            print('1. Cor da bandeira  - custa 4 tentativas')
-            print('2. Letra da capital - custa 3 tentativas')
-            print('4. População        - custa 5 tentativas')
-            print('5. Continente       - custa 7 tentativas')
-            print('0. Sem dica')
-            print('----------------------------------------')
-            opcao_dica = input('Escolha sua opção [0|1|2|4|5]: ')
-            del(respostas_dica[3])
-            while opcao_dica not in respostas_dica:
-                print ('Opção inválida')
-                opcao_dica = input('Escolha sua opção [0|1|2|3|4|5]: ')
-            
+        opcao_dica = input('Escolha sua opção [0|1|2|3|4]: ')
+        respostas_dica = ['0','1','2','3','4','5']
+        while opcao_dica not in respostas_dica:
+            print ('Opção inválida')
+            opcao_dica = input('Escolha sua opção [0|1|2|3|4]: ')
         if opcao_dica == '1':
+            if chances<=3:
+                print('Mercado de Dicas')
+                print('----------------------------------------')
+                print ('0. Sem dica')
+                print('----------------------------------------')
+                print('>>> Infelizmente, acabou seu estoque')
             chances -= 3
-                
+            for pais, dados in DADOS.items():
+                if pais == resposta_certa:
+                    for informacoes, valores in dados.items():
+                        if informacoes == 'bandeira':
+                            for cor,porcentagem in valores.items():
+                                if porcentagem > 0 and cor not in lista_dicas:
+                                    print('Dica:')
+                                    print(' - Cores da bandeira:{0}'.format(cor))
+                                    lista_dicas.append(cor)
+                                    break
         elif opcao_dica == '2':
             chances -= 2
-            
+            for pais, dados in DADOS.items():
+                if pais == resposta_certa:
+                    for informacoes, valores in dados.items():
+                        if informacoes == 'capital':
+                            for letras in valores:
+                                lista_letras_capital.append(letras)
+                            letra_escolhida = random.choice(lista_letras_capital)
+                            print('- Letras da capital: {0}'.format(letra_escolhida))
         elif opcao_dica == '3':
             chances -= 5
             # acessando a area
@@ -118,8 +115,7 @@ while chances > 0:
                             lista_dicas.append('-Continente: {}'.format(b))
             
         elif opcao_dica == '0':
-            print('Distâncias: \n {}'.format(lista_distancia))
-            print('Dicas: \n {}'.format(lista_dicas))
+            print('Dica: ')
             palpite = input('Qual seu palpite? ')
 
         print('Distâncias: \n {}'.format(lista_distancia))
@@ -138,6 +134,7 @@ while chances > 0:
 
     # CASO CHUTE NOME DE UM PAÍS
     else: 
+        #latitude e longitude
         for pais, dados in DADOS.items():
             if pais == resposta_certa:
                 for informacoes, valores in dados.items():
@@ -148,29 +145,15 @@ while chances > 0:
                             if lat_long == 'longitude':
                                 y1 = valores_lat_long
             if pais == palpite:
-                for informacoes2, valores2 in dados.items():
-                    if informacoes2 == 'geo':
-                        for lat_long2, valores_lat_long2 in valores2.items():
-                            if lat_long2 == 'latitude':
-                                x2 = valores_lat_long2
-                            if lat_long2 == 'longitude':
-                                y2 = valores_lat_long2
-        distancia = funcoes.haversine(raioterra, x1, y1, x2, y2)
-        lista_distancia = funcoes.adiciona_em_ordem(palpite, distancia, lista_distancia)
-
+                for item2, especificidades2 in pais.items():
+                    if item2 == 'latitude':
+                        x2 = especificidades2 
+                    if item2 == 'longitude':
+                        y2 = especificidades2 
+        distancia = funções.haversine(raioterra, x1, y1, x2, y2)
+        lista_distancia.append('{} km -> {}'.format(distancia, palpite)) #tem que por em ordem da menor distancia para maior
         print('Distâncias: \n {}'.format(lista_distancia))
-        print('Dicas: ')
-
-    # atualizando chances e tentativas ao final de cada rodada
-    chances -= 1
-    contador_tentativas += 1
-
-print('Até a próxima!')
-
-
-
-
-
+    
 {'afeganistao': 
 {'area': 652230, 'populacao': 31390200, 'capital': 'Cabul', 'geo': 
 {'latitude': 33.93911, 'longitude': 67.709953}, 
